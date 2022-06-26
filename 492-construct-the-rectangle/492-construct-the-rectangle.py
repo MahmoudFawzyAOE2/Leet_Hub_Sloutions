db15a6be44e72421c0546854e710fcb48ff29f5f
Mahmoud Fawzy Taha-Elaraby <76012086+MahmoudFawzyAOE2@users.noreply.github.com>
2022-06-27 00:21:25 +0200

    Time: 82 ms (27.16%), Space: 13.8 MB (58.22%) - LeetHub

--- 492-construct-the-rectangle/492-construct-the-rectangle.py
+++ 492-construct-the-rectangle/492-construct-the-rectangle.py
@@ -3,24 +3,14 @@ from math import sqrt
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         
-        w = sqrt(area)
-        
-        w = floor(w)
-         
+        w = floor(sqrt(area))
         
         for i in range(w):
-            
             L = area / w 
             
             if L % 1 == 0:
-                L = int(L)
-                break
-                
+                break   
             else:
                 w = w -1
                 
-        return [L,w]
-    
-    
-            
-            
+        return [int(L),w]   
